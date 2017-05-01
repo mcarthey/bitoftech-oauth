28d55027c2285d4e204fd6e7aa9b71b897ed396d
Mark McArthey <mcarthey@gmail.com>
2017-05-01 10:24:04 -0500

    Added ability to issue HTTP requests to generate tokens

---
+++ AngularJSAuthentication.API/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+
+namespace AngularJSAuthentication.API.Providers
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Responsible for validating the “Client”, in our case we have only one client so we’ll always return that its validated successfully
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        /// <summary>
+        /// Responsible to validate the username and password sent to the authorization server’s token endpoint, 
+        /// so we’ll use the “AuthRepository” class we created earlier and call the method “FindUser” to check 
+        /// if the username and password are valid
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // allow CORS on the token middleware provider - if you forget this, generating the token will fail when you try to call it from your browser
+            // Note this only allows CORS for token middleware provider not for ASP.NET Web API
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            // generate the token behind the scenes
+            context.Validated(identity);
+
+        }
+    }
+}
--- AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication.API/Startup.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using AngularJSAuthentication.API.Providers;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
@@ -13,11 +16,32 @@ namespace AngularJSAuthentication.API
     {
         public void Configuration(IAppBuilder app)
         {
+            // configure our API to use OAuth authentication workflow
+            ConfigureOAuth(app);
+
             // configure API routes
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
+            // Allow CORS for ASP.NET Web API
+            app.UseCors(CorsOptions.AllowAll);
             // wire up ASP.NET Web API to Owin server pipeline
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation - add the authentication middleware to the pipeline
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
