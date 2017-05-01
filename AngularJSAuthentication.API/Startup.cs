bc1ea3e79b74ea0a692fbb8cb30a9bb92a03e880
Mark McArthey <mcarthey@gmail.com>
2017-05-01 08:53:41 -0500

    Created basic infrastructure to register users into database via API without authentication

---
+++ AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // configure API routes
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            // wire up ASP.NET Web API to Owin server pipeline
+            app.UseWebApi(config);
+        }
+    }
+}
