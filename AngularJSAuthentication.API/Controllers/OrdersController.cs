ada9c7a5c2e11b927c025eaaf375958f9f8fa63e
Mark McArthey <mcarthey@gmail.com>
2017-05-01 09:52:05 -0500

    Added orders controller - currently not authorized - 301 unauthorized

---
+++ AngularJSAuthentication.API/Controllers/OrdersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AngularJSAuthentication.API.Models;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Order.CreateOrders());
+        }
+    }
+}
